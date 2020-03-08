fce8aa616b6e3056ab0e9fe18320abd7f37576d9
Girma Nigusse <xgirma@gmail.com>
2020-03-07 21:30:41 -0500

    update

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,16 +1,42 @@
-exports.config = {
-  directConnect: true,
-  specs: ["spec/*.spec.js"],
+const directConnect = true;
+const specs = ["spec/*.spec.js"];
+const framework = "jasmine";
+const jasmineNodeOpts = {
+  isVerbose: true,
+  realtimeFailure: true
+};
+
+const headed = {
+  directConnect,
+  specs,
   capabilities: {
     browserName: "chrome"
   },
-  framework: "jasmine",
-  jasmineNodeOpts: {
-    isVerbose: true,
-    realtimeFailure: true
-  },
+  framework,
+  jasmineNodeOpts,
   onPrepare: () => {
     /* eslint-disable-next-line global-require */
     require("esm");
   }
 };
+
+const headless = {
+  directConnect,
+  specs,
+  capabilities: {
+    browserName: "chrome",
+    chromeOptions: {
+      args: ["--headless", "--no-sandbox", "--disable-gpu"]
+    }
+  },
+  framework,
+  jasmineNodeOpts,
+  onPrepare: () => {
+    /* eslint-disable-next-line global-require */
+    require("esm");
+  }
+};
+
+const config = process.env.GITHUB_ACTIONS ? headless : headed;
+
+exports.config = config;
